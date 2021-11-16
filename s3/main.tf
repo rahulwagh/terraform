af3bd06b15295d102a39b09b70462fd089b68687
rwagh <rahul.wagh@crosskey.fi>
2021-11-16 21:01:50 +0200

    Added terrafrom s3 bucket code

---
+++ s3/main.tf
@@ -0,0 +1,13 @@
+
+resource "aws_s3_bucket" "spacelift-test1-s3" {
+  bucket = "spacelift-test1-s3"
+  acl = "private"
+}
+
+resource "aws_s3_bucket_object" "object1" {
+  for_each = fileset("uploads/", "*")
+  bucket = aws_s3_bucket.spacelift-test1-s3.id
+  key = each.value
+  source = "uploads/${each.value}"
+  etag = filemd5("uploads/${each.value}")
+}
