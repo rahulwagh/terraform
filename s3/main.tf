9740f5e31fdd6b0fb96862b9f7feb5d8b1e5c6a0
rwagh <rahul.wagh@crosskey.fi>
2021-11-16 21:48:58 +0200

    updated spacelift required provider

--- s3/main.tf
+++ s3/main.tf
@@ -1,9 +1,8 @@
 
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
+    spacelift = {
+      source = "downloads.spacelift.dev/spacelift-io/spacelift"
     }
   }
 }
